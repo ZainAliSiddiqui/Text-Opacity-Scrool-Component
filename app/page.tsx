33f342118418dcc74a0959121d5691037c800294
Zain Ali <ZainSoftwear11@gmail.com>
2023-12-26 23:20:58 +0500

    Rendered Home component

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,22 @@
+"use client";
+import { useEffect } from "react";
+import { Home } from "@components";
+import Lenis from "@studio-freight/lenis";
+
 export default function App() {
+	useEffect(() => {
+		const lenis = new Lenis();
+
+		function raf(time: any) {
+			lenis.raf(time);
+			requestAnimationFrame(raf);
+		}
+
+		requestAnimationFrame(raf);
+	}, []);
 	return (
-		<div>
-			<h1 className="text-[20rem]">App</h1>
-		</div>
+		<>
+			<Home />
+		</>
 	);
 }
